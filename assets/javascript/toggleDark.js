94d6ab688c1f9356ac8ae71b87d4eac2f5a188c3
Jared W <jwoytas@gmail.com>
2023-11-26 18:27:01 -0700

    Minor tweaks to dark toggle

--- assets/javascript/toggleDark.js
+++ assets/javascript/toggleDark.js
@@ -1,12 +1,21 @@
-var checkbox = document.querySelector('input[name=mode]')
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
+var darkToggle = function() {
+    if (this.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
         var moon = document.getElementById('moon')
-        moon.innerHTML = feather.icons['sun'].toSvg({'fill': 'var(--fg-color-dark)'})
+        moon.innerHTML = feather.icons['sun'].toSvg({ 'fill': 'var(--fg-color-dark)' })
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
         var moon = document.getElementById('moon')
-        moon.innerHTML = feather.icons['moon'].toSvg({'fill': 'var(--fg-color-dark)'})
+        moon.innerHTML = feather.icons['moon'].toSvg({ 'fill': 'var(--fg-color-dark)' })
     }
-})
+}
+
+var checkbox = document.querySelector('input[name=mode]')
+checkbox.addEventListener('change', darkToggle)
+
+checkbox.clicked = true;
+document.documentElement.setAttribute('data-theme', 'dark')
+var moon = document.getElementById('moon')
+moon.innerHTML = feather.icons['sun'].toSvg({ 'fill': 'var(--fg-color-dark)' })
+
+//checkbox.click();
